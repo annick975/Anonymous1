c6c6178f42724b15dce1f3d1e2a18f2b5d2529e3
NIYUBAHWE Annick <niyubahwe.annick975@gmail.com>
2025-03-16 20:55:45 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,1 @@
-import { FC } from "react";
 
-
-const Main: FC = () => {
-  return (
-    <div>
-     <h1 className="text-red-600 text-3xl">Annick</h1>
-    </div>
-  );
-};
-
-export default Main;
