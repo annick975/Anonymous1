c917f190416f4e266a3c42bb39abc38bc9d79cbc
annick975 <niyubahwe.annick975@gmail.com>
2025-03-16 08:23:05 +0200

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+
+
+const Main: FC = () => {
+  return (
+    <div>
+     <h1 className="text-red-600 text-3xl">Annick</h1>
+    </div>
+  );
+};
+
+export default Main;
